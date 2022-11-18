9bd91d1d15dcd47089703f0be5e5bd6e51b02271
DewaBiara <angabiara@gmail.com>
2022-11-18 18:57:54 +0800

    add test

---
+++ test/auth/login.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-undef */
+/* eslint-disable semi */
+const request = require('supertest');
+const app = require('../../app/index');
+
+describe('POST /v1/auth/login', () => {
+  const email = 'firman@gmail.com';
+  const password = 'firman';
+
+  it('should response with 201 as status code', async () => {
+    return request(app)
+      .post('/v1/auth/login')
+      .send({
+        email,
+        password,
+      })
+      .then((res) => {
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toEqual(res.body);
+      });
+  });
+});
---
+++ test/cars/listcars.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable semi */
+/* eslint-disable no-undef */
+const request = require('supertest');
+const app = require('../../app/index');
+
+describe('GET /v1/cars', () => {
+  it('should response with 200 as status code', async () => {
+    return request(app)
+      .get('/v1/cars')
+      .then((res) => {
+        expect(res.statusCode).toBe(res.statusCode);
+        expect(res.body).toEqual(res.body);
+      });
+  });
+});
